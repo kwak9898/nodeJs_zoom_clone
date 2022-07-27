375da1a460b35ababe7807f881c9a5da02a2e450
kwaktaemin <kwak9898@github.com>
2022-07-27 15:47:36 +0900

    server setting & HomePage Settings

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
-console.log("h")
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => res.render("home"));
+
+app.listen(3000, () => {
+    console.log("Listening on http://localhost:3000");
+});
