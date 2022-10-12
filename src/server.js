e959add36a7db50f814c2fa8a868b96e56a9b29e
kwaktaemin <kwak9898@github.com>
2022-10-12 10:54:58 +0900

    refactoring

--- src/server.js
+++ src/server.js
@@ -8,6 +8,5 @@ app.use("/public", express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000, () => {
-    console.log("Listening on http://localhost:3000");
-});
+const handelListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handelListen);
