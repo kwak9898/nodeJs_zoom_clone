2d00d0138daa1d0af8f356c9bcae88e9a4fa6d8c
kwaktaemin <kwak9898@github.com>
2022-10-17 12:34:43 +0900

    create message event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,11 +13,21 @@ function addMessage(message) {
     ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("input");
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${input.value}`);
+    });
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    const form = room.querySelector("form");
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -37,3 +47,7 @@ socket.on("welcome", () => {
 socket.on("bye", () => {
     addMessage("Someone left 😭");
 });
+
+socket.on("new_message", (msg) => {
+    addMessage(msg);
+})
--- src/server.js
+++ src/server.js
@@ -29,6 +29,10 @@ io.on("connection", socket => {
     socket.on("disconnecting", () => {
         socket.rooms.forEach(room => socket.to(room).emit("bye"));
     });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    })
 });
 
 /**
