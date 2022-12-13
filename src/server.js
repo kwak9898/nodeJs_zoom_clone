58ee4dc7bf32d419581a8779eeefd2bced94a09d
kwak9898 <kwak9898@gitbub.com>
2022-12-13 15:14:06 +0900

    Refactoring

--- src/server.js
+++ src/server.js
@@ -46,55 +46,37 @@ function countRoom(roomName) {
  */
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anonymous";
+
     socket.onAny(event => {
         console.log(wsServer.sockets.adapter);
         console.log(`Socket Event: ${event}`);
     });
+
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms());
     });
+
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => {
             socket.to(room).emit("bye", socket.nickname, countRoom(room) -1);
         });
     });
+
     socket.on("disconnect", () => {
         wsServer.sockets.emit("room_change", publicRooms());
     });
+
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
-    })
+    });
+
     socket.on("nickname", (nickname) => {
         socket["nickname"] = nickname;
     });
 });
-
-/**
- * Use WebSocket
- */
-// const wss = new WebSocket.Server({ server });
-// const sockets = [];
-//
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser ✅");
-//     socket.on("close", () => {
-//         console.log("Disconnected from the Browser ❌");
-//     });
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch (message.type) {
-//             case "new_message":
-//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//         }
-//     });
-// });
 
 HttpServer.listen(3000, handleListen);
