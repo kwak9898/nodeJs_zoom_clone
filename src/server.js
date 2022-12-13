e4072d2c5727324ef66627ef1f69c91a4d2a38ca
kwak9898 <kwak9898@gitbub.com>
2022-12-13 16:11:53 +0900

    User Video

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
-import { Server } from "socket.io";
+import SocketIo from "socket.io";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -14,69 +13,6 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const HttpServer = http.createServer(app);
-const wsServer = new Server(HttpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true,
-    },
-});
-
-instrument(wsServer, {
-    auth: false
-})
-
-function publicRooms() {
-    const sids = wsServer.sockets.adapter.sids;
-    const rooms = wsServer.sockets.adapter.rooms;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    })
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-/**
- * Use Socket.IO
- */
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anonymous";
-
-    socket.onAny(event => {
-        console.log(wsServer.sockets.adapter);
-        console.log(`Socket Event: ${event}`);
-    });
-
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => {
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) -1);
-        });
-    });
-
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-
-    socket.on("nickname", (nickname) => {
-        socket["nickname"] = nickname;
-    });
-});
+const wsServer = SocketIo(HttpServer);
 
 HttpServer.listen(3000, handleListen);
